7a7fc6657172769775c3c3a007038540c2b3a78f
Kwon Soonsun <kss_1162@naver.com>
2023-11-22 15:30:43 +0900

    이미지 등록 중

--- app/src/main/java/com/cookandroid/mystory/SaveFragment.java
+++ app/src/main/java/com/cookandroid/mystory/SaveFragment.java
@@ -2,7 +2,6 @@ package com.cookandroid.mystory;
 
 import android.os.Bundle;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.fragment.app.Fragment;
 
--- app/src/main/java/com/cookandroid/mystory/SelectFragment.java
+++ app/src/main/java/com/cookandroid/mystory/SelectFragment.java
@@ -1,9 +1,7 @@
 package com.cookandroid.mystory;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
@@ -11,10 +9,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 public class SelectFragment extends Fragment {
 
@@ -33,9 +28,6 @@ public class SelectFragment extends Fragment {
         listview2 = view.findViewById(R.id.listview2);
         searchView = view.findViewById(R.id.searchView);
 
-        String[] item = {"원", "투", "쓰리", "포", "원", "투", "쓰리", "포", "원", "투", "쓰리", "포", "원", "투", "쓰리", "포", "원", "투", "쓰리", "포"};
-
-        ArrayAdapter<String> list = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, item);
 
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
@@ -46,10 +38,6 @@ public class SelectFragment extends Fragment {
 
             @Override
             public boolean onQueryTextChange(String newText) {
-
-                list.getFilter().filter(newText);
-
-                listview2.setAdapter(list);
 
                 return false;
 
