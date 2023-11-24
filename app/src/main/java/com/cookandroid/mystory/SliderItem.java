58a70769301f67a983aa5b42221f215c189a80de
Kwon Soonsun <kss_1162@naver.com>
2023-11-24 23:48:59 +0900

    viewpage2 완성 (유튜브 참고)

---
+++ app/src/main/java/com/cookandroid/mystory/SliderItem.java
@@ -0,0 +1,15 @@
+package com.cookandroid.mystory;
+
+public class SliderItem {
+
+    private int image;
+
+    public SliderItem(int image) {
+        this.image = image;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+}
--- app/src/main/java/com/cookandroid/mystory/fragment/HomeFragment.java
+++ app/src/main/java/com/cookandroid/mystory/fragment/HomeFragment.java
@@ -2,18 +2,31 @@ package com.cookandroid.mystory.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.CompositePageTransformer;
+import androidx.viewpager2.widget.MarginPageTransformer;
+import androidx.viewpager2.widget.ViewPager2;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.cookandroid.mystory.MainActivity;
 import com.cookandroid.mystory.R;
+import com.cookandroid.mystory.SliderAdapter;
+import com.cookandroid.mystory.SliderItem;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
+    private ViewPager2 viewPager2;
+    private Handler sliderHendler = new Handler();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -24,10 +37,68 @@ public class HomeFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
+        viewPager2 = view.findViewById(R.id.viewPageImageSlider);
+
+        view();
+
+        CompositePageTransformer compositePageTransformer = new CompositePageTransformer();
+        compositePageTransformer.addTransformer(new MarginPageTransformer(40));
+        compositePageTransformer.addTransformer(new ViewPager2.PageTransformer() {
+            @Override
+            public void transformPage(@NonNull View page, float position) {
+                float r = 1 - Math.abs(position);
+                page.setScaleY(0.85f + r * 0.15f);
+            }
+        });
+
+        viewPager2.setPageTransformer(compositePageTransformer);
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                sliderHendler.removeCallbacks(sliderRunnable);
+                sliderHendler.postDelayed(sliderRunnable, 3000);
+            }
+        });
 
         return view;
     }
 
+    private Runnable sliderRunnable = new Runnable() {
+        @Override
+        public void run() {
+            viewPager2.setCurrentItem(viewPager2.getCurrentItem() + 1);
+        }
+    };
 
+    @Override
+    public void onPause() {
+        super.onPause();
+        sliderHendler.removeCallbacks(sliderRunnable);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        sliderHendler.postDelayed(sliderRunnable, 3000);
+
+    }
+
+    private void view() {
+        List<SliderItem> sliderItems = new ArrayList<>();
+
+        sliderItems.add(new SliderItem(R.drawable.image));
+        sliderItems.add(new SliderItem(R.drawable.image2));
+        sliderItems.add(new SliderItem(R.drawable.image3));
+        sliderItems.add(new SliderItem(R.drawable.image4));
+        sliderItems.add(new SliderItem(R.drawable.image5));
+
+        viewPager2.setAdapter(new SliderAdapter(sliderItems, viewPager2));
+
+        viewPager2.setClipToPadding(false);
+        viewPager2.setClipChildren(false);
+        viewPager2.setOffscreenPageLimit(3);
+        viewPager2.getChildAt(0).setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
+    }
 
 }
